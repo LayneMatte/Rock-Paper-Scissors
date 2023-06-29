02d9d509b23a2b4e55b242e572319e39cafa3650
LayneMatte <layne.matte@gmail.com>
2023-06-29 16:14:18 -0500

    First Draft

--- game.js
+++ game.js
@@ -24,49 +24,86 @@ This will be for the computers turn */
                      choice === "paper" && computerChoice === "paper" ||
                      choice === "scissors" && computerChoice === "scissors") {return resultText = "TIE"}
         }
+let win = document.getElementsByClassName('player-counter');
+function tempGreen () {
+win[0].style.cssText = `
+    animation: greenGrow .5s;
+    animation-timing-function: ease-in;`
+}
+let lose = document.getElementsByClassName('comp-counter');
+function tempRed () {
+lose[0].style.cssText = `
+ animation: redGrow .5s;
+ animation-timing-function: ease-in;
+ `
+}
+function tempYellow () {
+win[0].style.cssText = `
+animation: yellowGrow .5s;
+animation-timing-function: ease-in;` 
+lose[0].style.cssText = `
+animation: yellowGrow .5s;
+animation-timing-function: ease-in;
+`
+}
+function removeRedGreen () {
+    win[0].removeAttribute('style');
+    lose[0].removeAttribute('style');
+    }
+// initialize two global variables for player and comp score to be used later to add score 
+let playerscore = 0;
+let computerscore = 0;
 // create a nodelist containing all buttons 
 const choices = document.querySelectorAll('button');
 // iterate throught the nodelist adding an event listener to each buttont that listens for a click to fire off the play function 
 // store that iterator into a variable 
 let choice = choices.forEach(button => button.addEventListener('click',play));
+// let clear = choices.forEach(clear => clear.addEventListener('click', () => win[0].style.cssText = 'background-color:white'))
 // create the function that is called when one of the button is clicked 
 function play(e) {
+    test = document.getElementsByClassName('counter-container');
     // e.target.className pulls the class name for the clicked object 
     let choice = e.target.className;
-    console.log(choice)
-    //execute the above comparitive function passing the choice argument through 
+    // execute the above comparitive function passing the choice argument through 
     playRockPaperScissors (choice);
-    // select the outcome class using query selector 
-    let outcome = document.querySelector('.outcome');
-    //create an if statement for when there is already an answer that will delete the answer first 
-    if (outcome.childElementCount > 1) {
-        let unwantedChild = document.getElementsByClassName('outcome_text');
-        console.log(unwantedChild.nodeType)
-        outcome.removeChild(outcome.lastChild)}
-    else {let outcome_text = document.createElement('h2');
-    outcome_text.classList.add('outcome_text');
-    outcome_text.textContent = `${resultText}`;
-    outcome.appendChild(outcome_text);}}
+    // console.log(resultText)
+    // conditional to add to playerscore if player wins round 
+    if (resultText === "WIN") {
+    win[0].innerText = `Player Score: ${playerscore += 1}`;
+    tempGreen();
+    addEventListener('animationend',removeRedGreen)
+    }
+    // conditional to add to comp score if comp wins the round 
+        else if (resultText === "LOSE") {
+        lose[0].innerText = `Computer Score: ${computerscore += 1}`;
+        tempRed();
+        addEventListener('animationend',removeRedGreen)
 
-// BELOW TEXT WAS FOR EXERCISE THAT REQUIRED A FUNCTION TO EXECUTE 5 ROUNDS OF THE GAME AND PICK A WINNER BETWEEN PLAYER AND COMPUTER 
+        }
+        else if (resultText === "TIE") {
+            tempYellow();
+        }
+    // conditional to check for and declare the player won, as well as reset the score innertext as well as the global variable 
+    if (playerscore >= 3){
+    outcome = document.getElementsByClassName('outcome')
+    outcome[0].innerText = 'YOU WIN';
+    outcome[0].style.cssText = 'color:green; background-color: lightblue; border-radius: 10px; border: solid darkgrey 2px; padding: 2px;';
+    win[0].innerText = `Player Score: 0`;
+    lose[0].innerText = `Computer Score: 0`;
+    return playerscore = 0, computerscore = 0;
+}
+        // conditional to check for and declare the player lost, as well as reset the score innertext as well as the global variable 
+        else if (computerscore >= 3){
+        outcome = document.getElementsByClassName('outcome');
+        outcome[0].innerText = 'YOU LOSE';
+        outcome[0].style.cssText = 'color:red; background-color: beige; border-radius: 10px; border: solid darkgrey 2px; padding: 2px;'
+        win[0].innerText = `Player Score: 0`;
+        lose[0].innerText = `Computer Score: 0`;
+        return playerscore = 0, computerscore = 0;
+        }
+    }
 
-        // function alertFunction () {
-//     buttons.addEventListener(click,() => (alert('hey')))
-// }
- /* create function that executes 5 game rounds and decides a winner based on who wins the most that also does not include the ties or not valid options*/
-    // function game () {
-    //     let playerScore = 0;
-    //     let computerScore = 0;
-    //     for (i = 0, outcome >= 1; i < 5 ; i++)
-    //         {playRockPaperScissors()
-    //         if (outcome === 1) {playerScore++}
-    //         else if (outcome === 2) {computerScore++}
-    //         else if (outcome === 3) {console.log("no win!")}}
-    //     if (playerScore > computerScore) {console.log("YOU WIN!")}
-    //     else {console.log("YOU LOSE")}
-    // } 
-/* */ 
-/* */ 
-/* */ 
+
+
 
 
